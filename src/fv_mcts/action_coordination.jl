ecd2a744de2b363bfa4f55f7c7aa4bf59e3fee74
Shushman <shushmanchoudhury@gmail.com>
2019-12-17 23:32:19 -0800

    Updated Vec{S} to SV, more progress

--- src/fv_mcts/action_coordination.jl
+++ src/fv_mcts/action_coordination.jl
@@ -1,1 +1,9 @@
 ## Variable Elimination with the Coordination Graph
+
+# This is just for retrieving the best action at the end of planning
+function varel_action end
+
+# This is to compute the best action for UCB
+function varel_action_UCB end
+
+function varel_value end
--- src/fv_mcts/fv_mcts_vanilla.jl
+++ src/fv_mcts/fv_mcts_vanilla.jl
@@ -1,14 +1,15 @@
-## NOTE: We can use the following directly without modification
+## We can use the following directly without modification
 ## 1. domain_knowledge.jl for Rollout, init_Q and init_N functions
 ## 2. MCTSSolver for representing the overall MCTS (underlying things will change)
 
 
 # JointMCTS tree has to be different, to efficiently encode Q-stats
-mutable struct JointMCTSTree{S}
+# SV is the hashtype for state vector
+mutable struct JointMCTSTree{S,SV}
 
     # To track if state node in tree already
     # TODO: Is this acceptable? I prefer this so I can explicitly track the vector type
-    state_map::Dict{AbstractVector{S},Int64}
+    state_map::Dict{SV,Int64}
 
     # these vectors have one entry for each state node
     # Only doing factored satistics (for actions), not state components
@@ -29,11 +30,10 @@ end
 function JointMCTSTree(joint_mdp::JointMDP{S,A},
                        coord_graph_components::Vector{Vector{Int64}},
                        init_state::AbstractVector{S},
-                       init_Q = 0.0,
-                       init_N = 0,
                        sz::Int64=1000) where {S, A}
 
-    state_vec_type = statetype(joint_mdp)
+    # TODO: add to requirements
+    svht = state_vec_hash_type(joint_mdp)
 
     # Initialize full agent actions
     # NOTE: This can be more efficient if all agents have same actions...still, it's linear not exp
@@ -73,14 +73,84 @@ function JointMCTSTree(joint_mdp::JointMDP{S,A},
     end # for comp in n_comps
 
 
-    return JointMCTSTree{S}(Dict{state_vec_type,Int64}(),
+    return JointMCTSTree{S,svht}(Dict{svht,Int64}(),
 
-                            sizehint!(Vector{Int}[], sz),
-                            sizehint!(Int[], sz),
-                            sizehint!(S[], sz),
+                                sizehint!(Vector{Int}[], sz),
+                                sizehint!(Int[], sz),
+                                sizehint!(S[], sz),
 
-                            coord_graph_components,
-                            n_component_stats,
-                            q_component_stats
-                           )
+                                coord_graph_components,
+                                n_component_stats,
+                                q_component_stats
+                                )
 end # function
+
+
+
+Base.isempty(t::JointMCTSTree) = isempty(t.state_map)
+state_nodes(t::JointMCTSTree) = (JointStateNode(t, id) for id in 1:length(t.total_n))
+
+struct JointStateNode{S,SV}
+    tree::JointMCTSTree{S,SV}
+    id::Int64
+end
+
+get_state_node(tree::JointMCTSTree, s) = StateNode(tree, s)
+
+# accessors for state nodes
+@inline state(n::JointStateNode) = n.tree.s_labels[n.id]
+@inline total_n(n::JointStateNode) = n.tree.total_n[n.id]
+@inline child_ids(n::JointStateNode) = n.tree.child_ids[n.id]
+
+## No need for `children` or ActionNode just yet
+
+mutable struct JointMCTSPlanner{S, A, SV, SE, RNG <: AbstractRNG} <: AbstractMCTSPlanner{JM}
+    solver::MCTSSolver
+    mdp::JointMDP{S,A}
+    tree::JointMCTSTree{S,SV}
+    solved_estimate::SE
+    rng::RNG
+end
+
+# NOTE: JointMCTS Planner gets the adjacency matrix as argument
+function JointMCTSPlanner(solver::MCTSSolver,
+                          mdp::JointMDP{S,A},
+                          coord_adj_mat::Matrix{Int64},
+                          init_state::S
+                         ) where {S,A}
+
+    # Get coord graph comps from maximal cliques of graph
+    @assert size(coord_adj_mat)[1] == n_agents(mdp) "Adjacency Mat does not match number of agents!"
+    coord_graph_components = maximal_cliques(SimpleGraph(coord_adj_mat))
+
+    # Create tree FROM CURRENT STATE
+    tree = JointMCTSTree(mdp, coord_graph_components, init_state, solver.n_iterations)
+    se = convert_estimator(solver.estimate_value, solver, mdp)
+
+    return JointMCTSPlanner(solver, mdp, tree, se, solver.rng)
+end # end JointMCTSPlanner
+
+function clear_tree!(p::JointMCTSPlanner) p.tree = nothing end
+
+function get_state_node(tree::JointMCTSTree, s, planner::JointMCTSPlanner)
+    # TODO: Add hash to req
+    shash = hash(s)
+
+    if haskey(tree.state_map, shash)
+        return StateNode(tree, tree.state_map[shash]) # Is this correct? Not equiv to vanilla
+    else
+        return insert_node!(tree, planner, s)
+    end
+end
+
+# no computation is done in solve - the solver is just given the mdp model that it will work with
+POMDPs.solve(solver::MCTSSolver, mdp::JointMDP) = JointMCTSPlanner(solver, mdp)
+
+# IMP: Overriding action for JointMCTSPlanner here
+function POMDPs.action(p::JointMCTSPlanner, s)
+    tree = plan!(p, s)
+    return varel_action(tree) # TODO: Need to implement
+end
+
+## Not implementing value functions right now....
+## ..Is it just the MAX of the best action, rather than argmax???
